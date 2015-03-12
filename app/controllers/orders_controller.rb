1a91d5c1821837a74e6849078ed73868436be644
Justen Morgan <justenamorgan@gmail.com>
2015-03-11 22:10:01 -0700

    Pagination complete

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,7 +4,7 @@ class OrdersController < ApplicationController
   # GET /orders
   # GET /orders.json
   def index
-    @orders = Order.all
+    @orders = Order.paginate(:per_page =>10, page: params[:page]).order('created_at desc')
     respond_to do |format|
       format.html #index.html.erb
       format.json {render json: @orders}
